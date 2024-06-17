749bec1c68f8dd1623b7b25c60d42c8b7ceefb30
Suman Adhikari <expertxkid@gmail.com>
2024-06-17 17:39:08 -0500

    refactor: add lombok annotation in LocationType

--- src/main/java/edu/miu/attendance/domain/LocationType.java
+++ src/main/java/edu/miu/attendance/domain/LocationType.java
@@ -1,11 +1,15 @@
 package edu.miu.attendance.domain;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name = "LocationType")
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class LocationType {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
