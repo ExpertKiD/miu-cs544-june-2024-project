e7cb81bf71da8dd0535643f3c79ab19ce4f1fadf
Umar <ubunyaminu@miu.edu>
2024-06-18 18:14:44 -0500

    Modified ModelMapperConfig to handle getting location information

--- src/main/java/edu/miu/attendance/config/ModelMapperConfig.java
+++ src/main/java/edu/miu/attendance/config/ModelMapperConfig.java
@@ -1,6 +1,9 @@
 package edu.miu.attendance.config;
 
+import edu.miu.attendance.domain.AttendanceRecord;
+import edu.miu.attendance.dto.AttendanceRecordDTO;
 import org.modelmapper.ModelMapper;
+import org.modelmapper.PropertyMap;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -9,6 +12,15 @@ public class ModelMapperConfig {
 
     @Bean("modelMapper")
     public ModelMapper getModelMapper(){
-        return new ModelMapper();
+        ModelMapper modelMapper = new ModelMapper();
+        modelMapper.addMappings(new PropertyMap<AttendanceRecord, AttendanceRecordDTO>() {
+            @Override
+            protected void configure() {
+                map().setLocationName(source.getLocation().getName());
+                map().setLocationType(source.getLocation().getLocationType().getType());
+                map().setCourseOfferingName(source.getCourseOffering().getCourse().getName());
+            }
+        });
+        return modelMapper;
     }
 }
