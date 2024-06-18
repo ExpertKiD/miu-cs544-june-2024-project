d9fde5d7f8459ceca90127435fe66a6aa848daa4
AungKyawMyint <52407468+AungKyaw-Myint@users.noreply.github.com>
2024-06-17 22:31:30 -0500

    Course Offering
    adding CRUD of courseOffering

---
+++ src/main/java/edu/miu/attendance/controller/CourseOfferingController.java
@@ -0,0 +1,50 @@
+package edu.miu.attendance.controller;
+
+import edu.miu.attendance.dto.CourseOfferingDto;
+import edu.miu.attendance.service.CourseOfferingServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/v1")
+public class CourseOfferingController {
+
+    @Autowired
+    private CourseOfferingServiceImpl courseOfferingService;
+
+    @GetMapping("/student-view/course-offerings/{offeringId}")
+    public ResponseEntity<?> getCourseOfferingsById(@PathVariable long offeringId){
+        CourseOfferingDto courseOfferingDto=courseOfferingService.findById(offeringId);
+        return ResponseEntity.ok(courseOfferingDto);
+    }
+
+    @GetMapping("/sys-admin/course-offerings")
+    public ResponseEntity<Page<?>> getAllCourseOfferings(Pageable pageable){
+        Page<CourseOfferingDto> courseOfferingDto = courseOfferingService.findAll(pageable);
+        return ResponseEntity.ok(courseOfferingDto);
+
+    }
+
+    @PostMapping("/sys-admin/course-offerings")
+    public ResponseEntity<?> createCourseOffering(@RequestBody CourseOfferingDto courseOfferingDto){
+        CourseOfferingDto result=courseOfferingService.saveCourseOffering(courseOfferingDto, null );
+        return ResponseEntity.ok(result);
+    }
+
+    @PutMapping("/sys-admin/course-offerings/{offeringId}")
+    public ResponseEntity<?> updateCourseOffering(@PathVariable("offeringId") Long courseOfferingId, @RequestBody CourseOfferingDto courseOfferingDto){
+        CourseOfferingDto result=courseOfferingService.saveCourseOffering(courseOfferingDto,courseOfferingId);
+        return ResponseEntity.ok(result);
+    }
+
+
+    @DeleteMapping("/sys-admin/course-offerings/{offeringId}")
+    public ResponseEntity<?> deleteCourseOffering(@PathVariable long offeringId){
+        CourseOfferingDto courseOfferingDto=courseOfferingService.deleteCourseOffering(offeringId);
+        return ResponseEntity.ok(courseOfferingDto);
+    }
+
+}
---
+++ src/main/java/edu/miu/attendance/repository/FacultyRepository.java
@@ -0,0 +1,9 @@
+package edu.miu.attendance.repository;
+
+import edu.miu.attendance.domain.Faculty;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface FacultyRepository extends JpaRepository<Faculty, Long> {
+}
