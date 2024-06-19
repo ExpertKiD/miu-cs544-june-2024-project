d52535c74a134fac33f31c55498fb1a5a8f1d098
Suman Adhikari <expertxkid@gmail.com>
2024-06-19 13:58:45 -0500

    refactor/EA2: add /api/v1 to request path

--- src/main/java/edu/miu/attendance/controller/CourseController.java
+++ src/main/java/edu/miu/attendance/controller/CourseController.java
@@ -12,17 +12,20 @@ public class CourseController {
     private CourseService courseService;
 
     @PostMapping("/courses")
-    public ResponseEntity<?> save (@RequestBody CourseDTO courseDTO) {
+    public ResponseEntity<?> save(@RequestBody CourseDTO courseDTO) {
         return courseService.addCourses(courseDTO);
     }
+
     @GetMapping("/courses/{id}")
-    public  ResponseEntity<?> singleCourse (@PathVariable("id") Long id) {
+    public ResponseEntity<?> singleCourse(@PathVariable("id") Long id) {
         return courseService.getById(id);
     }
+
     @GetMapping("/courses")
-    public ResponseEntity<?> allCourse () {
+    public ResponseEntity<?> allCourse() {
         return courseService.getAllCourse();
     }
+
     @PutMapping("/courses")
     public ResponseEntity<?> updateCourse(@RequestBody CourseDTO courseDTO) {
         return courseService.updateCourse(courseDTO);
