bed4972c40cd60b7fe64f34a61734bb4bafe0c99
Suman Adhikari <expertxkid@gmail.com>
2024-06-19 14:17:22 -0500

    refactor/EA2: add and implement getCourseOfferingAttendanceByStudentId(studentId,courseOfferingId) in CourseOfferingService

--- src/main/java/edu/miu/attendance/service/CourseOfferingService.java
+++ src/main/java/edu/miu/attendance/service/CourseOfferingService.java
@@ -1,13 +1,19 @@
 package edu.miu.attendance.service;
 
 import edu.miu.attendance.dto.CourseOfferingDto;
+import edu.miu.attendance.dto.CourseOfferingStudentAttendanceDTO;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
 public interface CourseOfferingService {
 
     Page<CourseOfferingDto> findAll(Pageable pageable);
-    CourseOfferingDto findById(long id) ;
-    CourseOfferingDto saveCourseOffering(CourseOfferingDto courseOfferingDto,Long courseOfferingId);
+
+    CourseOfferingDto findById(long id);
+
+    CourseOfferingDto saveCourseOffering(CourseOfferingDto courseOfferingDto, Long courseOfferingId);
+
     CourseOfferingDto deleteCourseOffering(long id);
+
+    CourseOfferingStudentAttendanceDTO getCourseOfferingAttendanceByStudentId(String studentId, Long courseOfferingId);
 }
--- src/main/java/edu/miu/attendance/service/CourseOfferingServiceImpl.java
+++ src/main/java/edu/miu/attendance/service/CourseOfferingServiceImpl.java
@@ -1,15 +1,12 @@
 package edu.miu.attendance.service;
 
-import edu.miu.attendance.domain.Course;
-import edu.miu.attendance.domain.CourseOffering;
-import edu.miu.attendance.domain.Faculty;
-import edu.miu.attendance.domain.Session;
+import edu.miu.attendance.domain.*;
+import edu.miu.attendance.dto.AttendanceRecordDTO;
 import edu.miu.attendance.dto.CourseOfferingDto;
+import edu.miu.attendance.dto.CourseOfferingStudentAttendanceDTO;
+import edu.miu.attendance.dto.SessionDto;
 import edu.miu.attendance.exception.ResourceNotFoundException;
-import edu.miu.attendance.repository.CourseOfferingRepository;
-import edu.miu.attendance.repository.CourseRepository;
-import edu.miu.attendance.repository.FacultyRepository;
-import edu.miu.attendance.repository.SessionRepository;
+import edu.miu.attendance.repository.*;
 import jakarta.transaction.Transactional;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,7 +14,11 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+import java.util.stream.Collectors;
 
 @Service
 public class CourseOfferingServiceImpl implements CourseOfferingService {
@@ -36,32 +37,35 @@ public class CourseOfferingServiceImpl implements CourseOfferingService {
 
     @Autowired
     private SessionRepository sessionRepository;
+    @Autowired
+    private StudentRepository studentRepository;
 
     public Page<CourseOfferingDto> findAll(Pageable pageable) {
         return courseOfferingRepository.findAll(pageable).map(courseOffering -> modelMapper.map(courseOffering, CourseOfferingDto.class));
     }
 
     public CourseOfferingDto findById(long id) {
-        CourseOffering courseOffering=courseOfferingRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Course doesn't exit!"));;
+        CourseOffering courseOffering = courseOfferingRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Course doesn't exit!"));
+        ;
         return modelMapper.map(courseOffering, CourseOfferingDto.class);
     }
 
     @Transactional
-    public CourseOfferingDto saveCourseOffering(CourseOfferingDto courseOfferingDto,Long courseOfferingId) {
+    public CourseOfferingDto saveCourseOffering(CourseOfferingDto courseOfferingDto, Long courseOfferingId) {
         CourseOffering courseOffering;
-        if(courseOfferingId!=null){
-            courseOffering=courseOfferingRepository.findById(courseOfferingId).orElseThrow(() -> new ResourceNotFoundException("CourseOffering doesn't exit!"));
-            courseOffering=modelMapper.map(courseOfferingDto, CourseOffering.class);
+        if (courseOfferingId != null) {
+            courseOffering = courseOfferingRepository.findById(courseOfferingId).orElseThrow(() -> new ResourceNotFoundException("CourseOffering doesn't exit!"));
+            courseOffering = modelMapper.map(courseOfferingDto, CourseOffering.class);
             courseOffering.setId(courseOfferingId);
-        }else{
-            courseOffering=modelMapper.map(courseOfferingDto, CourseOffering.class);
+        } else {
+            courseOffering = modelMapper.map(courseOfferingDto, CourseOffering.class);
         }
 
 
-        Course course=courseRepository.findById(courseOfferingDto.getCourse_id()).orElseThrow(() -> new ResourceNotFoundException("Course doesn't exit!"));
-        Faculty faculty= facultyRepository.findById(courseOfferingDto.getFaculty_id()).orElseThrow(() -> new ResourceNotFoundException("Faculty doesn't exit!"));
-        List<Session> sessions=sessionRepository.findAllById(courseOfferingDto.getSessions_id());
-        if(sessions.isEmpty() || sessions.size()==0 || courseOfferingDto.getSessions_id().size()!=sessions.size()) {
+        Course course = courseRepository.findById(courseOfferingDto.getCourse_id()).orElseThrow(() -> new ResourceNotFoundException("Course doesn't exit!"));
+        Faculty faculty = facultyRepository.findById(courseOfferingDto.getFaculty_id()).orElseThrow(() -> new ResourceNotFoundException("Faculty doesn't exit!"));
+        List<Session> sessions = sessionRepository.findAllById(courseOfferingDto.getSessions_id());
+        if (sessions.isEmpty() || sessions.size() == 0 || courseOfferingDto.getSessions_id().size() != sessions.size()) {
             throw new ResourceNotFoundException("Session doesn't exit!");
         }
         courseOffering.setCourse(course);
@@ -75,9 +79,60 @@ public class CourseOfferingServiceImpl implements CourseOfferingService {
     @Override
     @Transactional
     public CourseOfferingDto deleteCourseOffering(long id) {
-        CourseOffering deletedData=courseOfferingRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Data doesn't exit!"));
+        CourseOffering deletedData = courseOfferingRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Data doesn't exit!"));
         courseOfferingRepository.deleteById(id);
 
         return modelMapper.map(deletedData, CourseOfferingDto.class);
+    }
+
+    @Override
+    public CourseOfferingStudentAttendanceDTO getCourseOfferingAttendanceByStudentId(String studentId, Long courseOfferingId) {
+        Student student =
+                studentRepository.findStudentByStudentId(studentId).orElseThrow(() -> new ResourceNotFoundException("Student with id #" + studentId + " doesn't exist"));
+
+        CourseOffering courseOffering =
+                student.getCoursesRegistrations()
+                        .stream()
+                        .filter(co -> Objects.equals(co.getId(), courseOfferingId))
+                        .findFirst()
+                        .orElseThrow(() -> new ResourceNotFoundException("Student is not " + "enrolled in course offered by ID #" + courseOfferingId));
+
+        final LocalDateTime now = LocalDateTime.now();
+
+        List<Session> activeSessions =
+                courseOffering.getSessions().stream()
+                        .filter(session -> session.getSessionDate().atTime(session.getEndTime()).isBefore(now))
+                        .toList();
+
+        Map<Long, Session> activeSessionsMap =
+                activeSessions.stream().collect(Collectors.toMap(Session::getId,
+                        session -> session));
+
+        var attendanceRecords = student.getAttendanceRecords().stream()
+                .filter(attendanceRecord -> {
+                    return attendanceRecord.getCourseOffering().getId().equals(courseOffering.getId()) &&
+                            activeSessionsMap.get(attendanceRecord.getSession().getId()) != null
+                            ;
+                }).toList();
+
+        if (activeSessions.isEmpty()) {
+            throw new ResourceNotFoundException("No active sessions found");
+        }
+
+        CourseOfferingStudentAttendanceDTO attendanceDTO =
+                new CourseOfferingStudentAttendanceDTO();
+
+        attendanceDTO.setAttendance(attendanceRecords
+                .stream()
+                .map((element) -> modelMapper.map(element, AttendanceRecordDTO.class))
+                .toList());
+
+        attendanceDTO.setSessions(activeSessions
+                .stream()
+                .map((element) -> modelMapper.map(element, SessionDto.class))
+                .toList());
+
+
+        return attendanceDTO;
     }
 }
